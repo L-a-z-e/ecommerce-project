29a9ae53e14c35de1e8d298a6ac8fb3d705f2d74
L-a-z-e <yysi8771@gmail.com>
2025-06-11 22:51:00 +0900

    application.properties -> .env 로 비밀번호 관리 & users 테이블 생성

---
+++ src/main/java/com/laze/ecommerceproject/domain/User.java
@@ -0,0 +1,34 @@
+package com.laze.ecommerceproject.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 ID 자동 생성 및 관리 auto-increment
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "point")
+    private int point = 0; // 기본값 지정
+}
